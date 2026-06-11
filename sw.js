11a953a10b810f5b3f67f0721340ac487367f054
mdomithasan6767-sys <mdomithasan6767@gmail.com>
2026-06-11 14:33:54 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
+const CACHE_NAME = 'sportzfy-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon.png'];
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css'
-      ]);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
