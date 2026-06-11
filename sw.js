b5b4bd7b81376e1878c25ccd304c3b00546da744
mdomithasan6767-sys <mdomithasan6767@gmail.com>
2026-06-11 16:49:59 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = 'sportzfy-tv-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  'https://i.postimg.cc/R0xj2GXq/Picsart-26-06-03-23-41-20-367.jpg'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    console.log('Service Worker installed');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(fetch(event.request));
 });
